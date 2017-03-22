f4bcdbb374f314226d90dfb8ded5efbb83010188
mosen <mosen1@gmail.com>
2017-03-22 21:57:16 +1100

    Initial Commit

---
+++ jss-uapi/jss-uapi.h
@@ -0,0 +1,19 @@
+//
+//  jss-uapi.h
+//  jss-uapi
+//
+//  Created by Administrator on 22/3/17.
+//  Copyright © 2017 Mosen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for jss-uapi.
+FOUNDATION_EXPORT double jss_uapiVersionNumber;
+
+//! Project version string for jss-uapi.
+FOUNDATION_EXPORT const unsigned char jss_uapiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <jss_uapi/PublicHeader.h>
+
+
